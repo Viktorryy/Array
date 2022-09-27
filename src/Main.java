7d8ab5f9cd68be72b50f734507d140ae4574c9d9
Viktorryy <viktory_o@rambler.ru>
2022-09-27 18:50:23 +0400

    //  Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        //  Задание 1
+
+        int [] mass1 = new int[] {1, 2, 3};
+
+        float [] mass2 = {1.57F, 7.654F, 9.986F};
+
+        int [] mass3 = new int[3];
+        mass3 [0] = 5;
+        mass3 [1] = 15;
+        mass3 [2] = 25;
+
+        System.out.println("Все три массива объявлены!");
     }
 }
