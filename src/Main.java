96b9874b5d9052513515d82ac63fed7ccf220829
Viktorryy <viktory_o@rambler.ru>
2022-09-27 19:17:30 +0400

    //  Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args){
 
-        //  Задание 2
+        //  Задание 3
 
 
         int [] mass1 = new int[] {1, 2, 3};
-        for (int i = 0; i < mass1.length; i++) {
-            if (i < mass1.length - 1) {
+        for (int i = mass1.length-1; i >=0 ; i--) {
+            if (i > 0) {
                 System.out.print(mass1[i] + ", "); }
             else
             {
@@ -17,8 +17,8 @@ public class Main {
 
 
         float [] mass2 = {1.57F, 7.654F, 9.986F};
-        for (int i = 0; i < mass2.length; i++) {
-            if (i < mass2.length - 1) {
+        for (int i = mass2.length-1; i >=0 ; i--) {
+            if (i > 0) {
                 System.out.print(mass2[i] + ", "); }
             else {
                 System.out.print(mass2[i]);
@@ -31,8 +31,8 @@ public class Main {
         mass3 [1] = 15;
         mass3 [2] = 25;
 
-        for (int i = 0; i < mass3.length; i++) {
-            if (i < mass2.length - 1) {
+        for (int i = mass3.length-1; i >=0 ; i--) {
+            if (i > 0) {
                 System.out.print(mass3[i] + ", "); }
             else {
                 System.out.print(mass3[i]);
